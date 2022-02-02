34c978926772a0f2ef1cdc70bbce94c8a7fb5cb4
idan kelman <kelmanidan@gmail.com>
2022-02-02 12:24:23 +0200

    added chart data functionality

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -5,15 +5,36 @@ import { BarChart, ResponsiveContainer, AreaChart, Area, XAxis, YAxis, Tooltip, 
 function ChartComponent(params) {
 
     const range = 20;
+    const max_amount =15;
+
+
+
+    function RearrangeData(data,len)
+    {
+        let Custom_data= [];
+        let ratio = len<max_amount?1:parseInt(len/max_amount);
+        // ratio = 1;
+
+        for(let i =0 ,ind = 0;i<len;i+=ratio,ind++)
+            Custom_data.push(data[i]);
+        
+        while(Custom_data.length>max_amount)
+            Custom_data.pop();
+
+
+        return Custom_data;
+
+    }
 
     return (
         <div  className = "ChartHolder" id = "Charter">
             <ResponsiveContainer width="90%" height={500} >
-                <BarChart width="100%" height={300} data={params.data}>
+                <BarChart width="100%" height={300} data={RearrangeData(params.data,params.max_amount)}>
                     <Bar dataKey="high" fill="#8884d8" />
-                    <Bar dataKey="low" fill="#82ca9d" />
+                    {/* <Bar dataKey="low" fill="#82ca9d" /> */}
+                    {/* <Bar dataKey="low" fill="#82ca9d" /> */}
                     <XAxis dataKey="Time"></XAxis>
-                    <YAxis dataKey="high" domain={[params.data[0].low -range*10 ,params.data[0].high + range ]} ></YAxis>
+                    <YAxis dataKey="high" domain={[params.data[0].high -range*10 ,params.data[0].high + range ]} ></YAxis>
                     <Tooltip></Tooltip>
                 </BarChart>
             </ResponsiveContainer>
