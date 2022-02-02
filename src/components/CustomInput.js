5d0a9b404ec82b5501a3a6cb247e0af5f0188840
idan kelman <kelmanidan@gmail.com>
2022-02-02 10:23:57 +0200

    Custom react input componenet

---
+++ src/components/CustomInput.js
@@ -0,0 +1,45 @@
+import React, { useState, useRef } from 'react';
+
+function CustomInput(params) {
+
+
+    const ThisButton = useRef();
+
+    function SubmitHandler(event) {
+        // event.preventDefault(); // if we have a form that sends a submit http
+        // request , we want to cancel it and redirect it
+        // ourselves.
+
+        // const Val = this.current.value; //this way we can get the value of
+        //the elemtn. ofc we would like to make a
+        //textbox or something like a checkbox , that will make more sense
+        //for the value , but this is the basic structure .
+        // console.log(Val);
+        // if(Val)
+        console.log("Changed");
+        console.log(ThisButton.current.value);
+        //if(String(ThisButton.current.value).length == 0)
+        //    console.log("empty");
+        let tmp = "" + ThisButton.current.value;
+        console.log(tmp.length);
+        if (tmp.length == 0)
+            ThisButton.current.value = 1;
+
+        return ThisButton.current.value;
+    }
+
+    return (
+
+        <div className="inputs">
+            <div className="Holder">
+                <input type="text" ref={ThisButton} onChange={SubmitHandler}></input>
+                <div className="Info">{params.Currency}</div>
+            </div>
+            <div className="Info">{params.text}</div>
+
+
+        </div>
+    );
+}
+
+export default CustomInput;
