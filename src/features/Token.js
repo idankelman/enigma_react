76b2abf11842231afb008032c8668ce2c8d379b1
idan kelman <kelmanidan@gmail.com>
2022-02-02 10:22:57 +0200

    reducer for the token feature in the store

---
+++ src/features/Token.js
@@ -0,0 +1,41 @@
+
+
+//==========================================================================
+//                          Imports and requires
+//==========================================================================
+
+
+import {createSlice} from '@reduxjs/toolkit'
+
+
+
+//==========================================================================
+//                          Create Slice Object for the Token
+//==========================================================================
+
+
+export const userSlice = createSlice({
+    name : "token",
+    initialState: {value : { low:2.0 , high:2.0 ,stamp:"",chart:2.0}},
+    reducers:
+    {
+        //create the token
+        create: (state,action)=>
+        {
+            //state holds info about current and previous values
+            //state is dynamic and can change 
+
+            //action , we can pass infomation that we want to use
+            //when the value is changed
+
+            state.value = action.payload;
+        } 
+    }
+});
+
+
+export const {create} = userSlice.actions;
+
+
+//exporting the infomataion that we careted so we can use it 
+export default userSlice.reducer;
