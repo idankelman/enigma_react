1aad5f2f2bd59dbce391337a9aecc67f7a936d23
idan kelman <kelmanidan@gmail.com>
2022-02-02 10:24:13 +0200

    custom loading component #2

---
+++ src/components/LoadComponent.js
@@ -0,0 +1,23 @@
+
+function LoadCompopnent(params) {
+
+    return (
+        <div className="Contain">
+            <div className="Loader">
+                <button className="button2" id = "one" ></button>
+                
+                <button className="button2" id = "two"></button>
+                
+                <button className="button2" id = "three"></button>
+
+                <button className="button2" id = "four"></button>
+                
+                <button className="button2" id = "five"></button>
+
+
+            </div>
+        </div>
+    );
+}
+
+export default LoadCompopnent;
