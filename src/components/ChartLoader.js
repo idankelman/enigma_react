586bd3122344887f8f885a6b2bdbe7ad0f9c3a64
idan kelman <kelmanidan@gmail.com>
2022-02-02 10:23:18 +0200

    custom loading animation

---
+++ src/components/ChartLoader.js
@@ -0,0 +1,16 @@
+function ChartLoader(params) {
+
+    return (
+        <div className="Contain">
+            <div id="loader">
+                <div id="box"></div>
+                <div id="hill"></div>
+            </div>
+        ד</div>
+    );
+}
+
+export default ChartLoader;
+
+
+
